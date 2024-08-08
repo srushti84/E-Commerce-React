1cb63287cdc0da62234e0c74dff07c40a87283d4
Srushti S <srushtirgs2004@gmail.com>
2024-08-08 10:44:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// react router
+// App.js
 import {
   Route,
   createRoutesFromElements,
@@ -9,10 +9,16 @@ import {
 // layout
 import RootLayout from "./layout/RootLayout";
 // pages
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import Home from "./pages/Home";
 import ExploreProduct from "./pages/ExploreProducts";
 import Product from "./pages/Product";
 import Checkout from "./pages/Checkout";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+import Contact from "./components/contact/Contact";
+import About from "./components/about/About";
+
 
 // react toast
 import { Toaster } from "react-hot-toast";
@@ -21,9 +27,13 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<RootLayout />}>
       <Route index element={<Home />} />
-      <Route path="/explore/:category" element={<ExploreProduct />}></Route>
-      <Route path="/product/:productId" element={<Product />}></Route>
-      <Route path="/checkout" element={<Checkout />}></Route>
+      <Route path="/explore/:category" element={<ExploreProduct />} />
+      <Route path="/product/:productId" element={<Product />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/about" element={<About />} />
     </Route>
   )
 );
