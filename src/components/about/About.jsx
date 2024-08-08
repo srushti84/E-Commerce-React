8a163f14698a94f925c92bbc266049547fd71d86
Srushti S <srushtirgs2004@gmail.com>
2024-08-08 10:39:07 +0530

    Create About.jsx

---
+++ src/components/about/About.jsx
@@ -0,0 +1,21 @@
+import './About.css';
+
+const About=()=>{
+    return(
+        <div className='about-wrapper'>
+            <div className='about-container'>
+                <h2>About Us</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto, aut perspiciatis iste iure maiores amet magni similique natus laudantium eaque, unde quo distinctio alias, rem autem recusandae expedita nesciunt eos.
+                Perspiciatis, ut laboriosam assumenda commodi tenetur quos dicta et voluptas vitae sunt atque dolor maxime ipsum nostrum voluptates cumque voluptatibus ea harum. Harum at numquam vero eveniet rerum ipsam! Modi?
+                Ipsam magni omnis laudantium error autem natus harum adipisci dolor esse, eaque pariatur cumque sapiente quod, laboriosam unde enim ullam ut saepe numquam rem! Quis cumque accusantium ut magnam repellendus.
+                Laudantium pariatur quisquam recusandae vero veniam quia temporibus numquam, voluptas quis? Delectus illo cum quia consectetur libero illum, accusamus esse quaerat excepturi deleniti iusto aliquid magni, atque minus quas modi.
+                Ad fugit asperiores corrupti! Laborum fuga libero ratione doloremque! Cumque, suscipit. Deserunt magni quis nesciunt nemo. Similique corporis minima eos qui. Commodi tenetur neque sequi mollitia consequatur iste voluptatibus accusantium.0</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis culpa aliquam, hic iusto illum ullam omnis consequatur ut qui quasi quisquam sunt eveniet delectus odio corporis minima, repudiandae recusandae natus.
+                Eveniet quidem molestias optio quas voluptatem doloremque magnam dolore voluptatum id velit adipisci perspiciatis eos vitae dolorem tempore, rem atque neque molestiae nemo similique consectetur accusamus distinctio autem? Eaque, praesentium.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default About;
